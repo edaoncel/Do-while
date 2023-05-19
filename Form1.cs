2bf19976b16621eb505f7aa3370469005ff34c5e
eda oncel <edaoncel@email.com>
2023-05-19 14:27:45 +0300

    Do-while Projesi

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace do__while
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sayac = 5;
+            while (sayac > 5 && sayac < 10)
+            {
+                sayac++;
+                MessageBox.Show(sayac.ToString());
+            }
+            do
+            {
+                sayac++;
+                MessageBox.Show(sayac.ToString());
+            }
+            while (sayac > 5 && sayac > 10);
+     
+        }
+    }
+}
